1a6a514d57cd1628b181538d42b740722d7efd6e
SantillanLautaroNahuel <santillanlautaronahuel@gmail.com>
2024-08-06 14:53:32 -0300

    crud done

--- backend/index.js
+++ backend/index.js
@@ -29,6 +29,40 @@ app.get('/books', async(req, res) => {
     }
 });
 
+app.get('/book/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const book = await Book.findById(id);
+
+        return res.status(200).json(book);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({message: error.message});
+    }
+});
+
+app.put('/book/:id', async(req, res)=>{
+    try {
+        if( !req.body.title && !req.body.author && !req.body.publishedYear)
+            {return res.status(400).send({message: "At least one field required"})}
+        
+        const {id} = req.params;
+        const result = await Book.findByIdAndUpdate(id, req.body);
+
+        if (!result) {
+            return response.status(404).send({message: 'Book not found'});
+        }
+
+        const updatedBook = await Book.findById(id);
+        return res.status(200).json(updatedBook);
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({message: error.message});
+    }
+});
+
 app.post('/newBook', async(req, res) => {
     try {
         if( !req.body.title || !req.body.author || !req.body.publishedYear)
@@ -45,6 +79,22 @@ app.post('/newBook', async(req, res) => {
     
     } catch (error) {
         console.log(error)
+        res.status(500).send({message: error.message});
+    }
+});
+
+app.delete('/book/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const result = await Book.findByIdAndDelete(id);
+
+        if (!result) {
+            return response.status(404).send({message: 'Book not found'});
+        }
+
+        return res.status(200).send({message: "Book deleted"});
+    } catch (error) {
+        console.log(error.message);
         res.status(500).send({message: error.message});
     }
 });
